e030c289951648874b6a2aaa9a94739e7ffa990a
Krisztian Litkey <kli@iki.fi>
2017-09-30 19:01:03 +0300

    .emacs: less eager ression-reload.

--- .emacs.d/01-stateful-session.el
+++ .emacs.d/01-stateful-session.el
@@ -1,5 +1,8 @@
 ; Restore all buffers lazily/on-demand.
-(setq desktop-restore-eager 5)
+(setq desktop-restore-eager 10)
+
+; Restore also frames.
+(setq desktop-restore-frames 't)
 
 ; Enable stateful sessions.
 (desktop-save-mode 1)
